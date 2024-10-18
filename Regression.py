9b9c06fdd153f95c9421005910ab27f6efcb735e
Johanna Bayer <hannabayer.89@gmail.com>
2024-10-18 12:02:20 +0200

    add loss function

--- Regression.py
+++ Regression.py
@@ -11,3 +11,10 @@ Y += np.random.normal(0.0, 0.5, size = nb_samples)
 
 plt.scatter(X, Y)
 plt.show()
+
+def loss(v):
+    e = 0.0
+    for i in range(nb_samples):
+        e += np.square(v[0] + v[1]*X[i] - Y[i])
+    return 0.5*e
+    
