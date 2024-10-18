19436ff33dcb7b90c882b2273a99eb6cff869e58
Johanna Bayer <hannabayer.89@gmail.com>
2024-10-18 12:23:18 +0200

    add regression

--- Regression.py
+++ Regression.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.optimize import minimize
 
 nb_samples = 200
 
@@ -10,11 +11,32 @@ Y = X + 2
 Y += np.random.normal(0.0, 0.5, size = nb_samples)
 
 plt.scatter(X, Y)
-plt.show()
+#plt.show()
 
+
+# functions
 def loss(v):
+    """_summary_
+
+    Args:
+        v (np_array): numpy array containing fitted parameters 
+
+    Returns:
+        scalar: summed loss
+    """
     e = 0.0
     for i in range(nb_samples):
-        e += np.square(v[0] + v[1]*X[i] - Y[i])
-    return 0.5*e
+        e += np.square(v[0] + v[1] * X[i] - Y[i])
+    return 0.5 * e
     
+    
+def gradient(v):
+    g = np.zeros(shape=2)
+    for i in range(nb_samples):
+        g[0] += (v[0] + v[1] * X[i] - Y[i])
+        g[1] += (v[0] + v[1] * X[i] - Y[i] * X[i])
+    return g
+
+
+result=minimize(fun=loss, x0=[0.0, 0.0], jac=gradient, method='Powell')
+print(result)
